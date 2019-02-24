e4d13639d6b4520b423dfae75dd6d4e7851ab852
Nguyen Phan <mangolionstudio@gmail.com>
2019-02-24 07:43:58 -0600

    mock interface

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ function ngMain()
   AppForm = new ngControls(maingui);
   AppForm.Update();
 }
+
+//add callback to after windows update (1 sec)
--- maingui.js
+++ maingui.js
@@ -1,1 +1,54 @@
-maingui = {};
+maingui = {
+    paneCamera: {
+      Type: 'wePanel',
+      L: 0,
+      T: 0,
+      R: 250,
+      B: 0,
+      style: {
+        borderStyle: 'dashed',
+        borderColor: 'gray'
+      }
+    },
+    btScanItems: {
+      Type: 'weButton',
+      T: 0,
+      W: 250,
+      R: 0,
+      Data: {
+        HTMLEncode: true,
+        Text: 'Scan Items'
+      }
+    },
+    listItems: {
+      Type: 'weList',
+      T: 32,
+      W: 250,
+      R: 0,
+      H: 200,
+      Data: {
+        HTMLEncode: true,
+        Items: []
+      },
+      style: {
+        borderBottom: ' dashed green',
+        borderTop: ' dashed green'
+      }
+    },
+    btCheckout: {
+      Type: 'weButton',
+      T: 238,
+      W: 250,
+      R: 0,
+      Data: {
+        HTMLEncode: true,
+        Text: 'Checkout'
+      }
+    }
+  }
+  
+  GUI = {};
+  GUI.setItems = function(items){
+      AppForm.listItems.setItems(items);
+      Appform.Update();
+  }
